b536c32f9688c4c4fbc96cd9e511903c034c36f0
taziamine95 <tazikacemamine95@gmail.com>
2023-10-19 10:22:40 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * isLower - determines whether ascil is lowercase
+ * isLower - determines whether ascii is lowercase
  * @c: character
  * Return: 1 if true, 0 if false
 */
@@ -21,7 +21,7 @@ int isLower(char c)
 int isDelimiter(char c)
 {
 	int i;
-	char delimiter[] = " \t\n,.!,\"(){}";
+	char delimiter[] = " \t\n,.!?\"(){}";
 
 	for (i = 0; i < 12; i++)
 		if (c == delimiter[i])
@@ -51,7 +51,7 @@ char *cap_string(char *s)
 		}
 		else
 			foudDelimit = 0;
-		s++
+		s++;
 	}
 	return (ptr);
 }
